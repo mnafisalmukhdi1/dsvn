2c6eec2b68022cc64cd125a5bebe2884aa5149b1
M. Nafis Al-Mukhdi <74487079+mnafisalmukhdi1@users.noreply.github.com>
2021-01-20 16:28:40 +0800

    Update options.js

--- js/options.js
+++ js/options.js
@@ -17,7 +17,7 @@ monogatari.settings({
 	// The name of your game, this will be used to store all the data so once
 	// you've released a game using one name, it shouldn't change. Please use the
 	// Version Setting to indicate a new release of your game!
-	'Name': 'My Visual Novel',
+	'Name': 'Detektif Sekolahan Visual Novel',
 
 	// The version of your game in semantic versioning (https://semver.org/).
 	'Version': '0.1.0',
