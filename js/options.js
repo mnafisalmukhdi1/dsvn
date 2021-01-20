6f1f11c52f90a98a388f8a9e21199aad8ba0c13b
M. Nafis Al-Mukhdi <74487079+mnafisalmukhdi1@users.noreply.github.com>
2021-01-20 15:58:28 +0800

    Update options.js

--- js/options.js
+++ js/options.js
@@ -17,13 +17,13 @@ monogatari.settings({
 	// The name of your game, this will be used to store all the data so once
 	// you've released a game using one name, it shouldn't change. Please use the
 	// Version Setting to indicate a new release of your game!
-	'Name': 'My Visual Novel',
+	'Name': 'Detektif Sekolahan',
 
 	// The version of your game in semantic versioning (https://semver.org/).
 	'Version': '0.1.0',
 
 	// Initial Label *
-	'Label': 'Start',
+	'Label': 'Mulai',
 
 	// Number of AutoSave Slots
 	'Slots': 10,
@@ -41,8 +41,8 @@ monogatari.settings({
 	'MainScreenMusic': '',
 
 	// Prefix for the Save Slots in Local Storage.
-	'SaveLabel': 'Save',
-	'AutoSaveLabel': 'AutoSave',
+	'SaveLabel': 'Simpan',
+	'AutoSaveLabel': 'Simpan Otomatis',
 
 	// Turn main menu on/off; Default: true *
 	'ShowMainScreen': true,
@@ -136,7 +136,7 @@ monogatari.settings({
 monogatari.preferences ({
 
 	// Initial Language for Multilanguage Games or for the Default GUI Language.
-	'Language': 'English',
+	'Language': 'Indonesia',
 
 	// Initial Volumes from 0.0 to 1.
 	'Volume': {
