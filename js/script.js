66d215dfe34d35a83cd713a7953c5b0f95b83c6e
M. Nafis Al-Mukhdi <74487079+mnafisalmukhdi1@users.noreply.github.com>
2021-01-21 16:13:24 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,30 +18,35 @@ monogatari.characters ({
 	'i' : {
 		name: 'Idris',
 		color: '#4682B4',
+		sprites: {normal: 'idris.png'},
 		expressions: {normal: 'idris.png'},
 		default_expression: 'normal'
 	},
 	'f' : {
 		name: 'Firdaus',
 		color: '#9ACD32',
+		sprites: {normal: 'firdaus.png'},
 		expressions: {normal: 'firdaus.png'},
 		default_expression: 'normal'
 	},
 	'm' : {
 		name: 'Mustafa',
 		color: '#00008B',
+		sprites: {normal: 'mustafa.png'},
 		expressions: {normal: 'mustafa.png'},
 		default_expression: 'normal'
 	},
 	'z' : {
 		name: 'Zain',
 		color: '#1E90FF',
+		sprites: {normal: 'mustafa.png'},
 		expressions: {normal: 'zain.png'},
 		default_expression: 'normal'
 	},
 	'b' : {
 		name: 'Bagus',
 		color: '#CD853F',
+		sprites: {normal: 'bagus.png'},
 		expressions: {normal: 'bagus.png'},
 		default_expression: 'normal'
 	}
@@ -55,7 +60,7 @@ monogatari.configuration ('credits', {
 	},
 	"Developers": {
 		"Artist": "AvatarMaker.com",
-		"Scenario Writer": "mnafisalmukhdi1",
+		"Writer": "mnafisalmukhdi1",
 	},
 });
 
@@ -82,8 +87,10 @@ monogatari.script ({
 		'i Aku memasuki sekolah ini karena terkena zonasi, rumahku yang tepat di perbatasan antara Kota Sukamawar dengan Kota Kebun Melati dikatakan lebih dekat ke MA Sukamawar daripada MA Kebun Melati.',
 		'i Padahal karena aku kelahiran Kebun Melati, tentu saja aku bersekolah di sana. Tapi sistem zonasi yang disepakati oleh kedua walikota memilihku untuk berada di Sukamawar.',
 		'i Aku belum mengenal Sukamawar sepenuhnya, kecuali namanya saja. Maka, aku orang baru di sini, dan aku dipenuhi dengan kebingungan.',
+		'show character b normal',
 		'i Pagi itu dilaksanakanlah upacara penaikan bendera. Dengan penyampai amanat seorang pria, kutebak itu kepala sekolah, karena beliau menyampaikan selamat datang dan membanggakan unggulnya MA Sukamawar.',
 		'i Dia mengingat-ingatkan prestasi zaman dulu, salah satu di antaranya adalah seorang siswa yang memenangkan olimpiade di Jepang.',
+		'hide character b',
 		'i Pembahasan ini sebenarnya menarik, namun aku merasa bosan. Kebosanan ini membuatku melalui waktu secara tidak sadar.',
 		'i Upacara selesai. Kami disuruh menuju ruang multiguna.',
 		'show background chairs',
@@ -98,5 +105,13 @@ monogatari.script ({
 		'm Setiap tahun, sekolah ini mengadakan Masa Taaruf Siswa Madrasah, atau yang disingkat Matsama. Acara ini diselenggarakan agar kita bisa mengenal, dari siswa sampai sekolah ini sendiri.',
 		'm Dan tahun ini, kami dipercaya sebagai penyelenggara. Doakan juga acara ini berjalan dengan lancar, semoga.',
 		'z Apa maksudnya?'
+		'z Ya, acara pertama kita selesai. Sepertinya kita juga tidak perlu sambutan kepala sekolah mengingat beliau yang menyampaikan amanat tadi. Maka tidak ada acara lagi selain berkenalan. Silahkan mulai dari teman di samping!',
+		'z Kalian diharuskan selalu berjalan beriringan dengan salah satu teman baru kalian, agar lebih memperat pertemanan kalian.',
+		'z Nanti juga kalian boleh menunjukkan bakat pada besok hari. Sampai jumpa.',
+		'i Aku sangat canggung saat itu, belum berani walau sebatas menyebutkan nama. Aku hanya terus menunduk.',
+		'Hai!',
+		'i Oh?',
+		'f Namaku Ahmad Firdaus. Namamu siapa?',
+		'i Namaku Muhammad Idris.'
 	],
 });
